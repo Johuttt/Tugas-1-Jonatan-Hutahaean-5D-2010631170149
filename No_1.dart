882e12c5159939f17b7d644b9b4c269993ee9dcc
Jonatan069 <2010631170149@student.unsika.ac.id>
2022-10-14 01:10:30 +0700

    Tugas 1_Jonatan Hutahaean_5D_2010631170149

---
+++ No_1.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // Variabel
+  String nama = "Jonatan Hutahaean";
+  int umur = 20;
+  bool bekerja = false;
+  var alamat = "Bekasi";
+  double tinggi = 172.56;
+  String status = "Jomblo";
+
+  // cetak variabel
+  print(
+      "Perkenalkan nama saya $nama, saya berumur $umur tahun. Saya tinggal di $alamat.");
+  print("Status: $status");
+  print("Tinggi Badan: $tinggi cm");
+  print("Status Bekerja: $bekerja");
+}
---
+++ No_3.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  List<String> matkul = [
+    "Data mining",
+    "Pemrograman Berbasis Mobile",
+    "Kalkulus",
+    "Technopreneur",
+    "Blockchain",
+  ];
+
+  Map<String, dynamic> dosen = {
+    'Nama': 'Jonatan',
+    'Hobi': 'Mancing',
+    'umur': 21
+  };
+
+  Set<int> angka = {1, 2, 3, 4, 5};
+
+  print(matkul);
+  print(dosen);
+  print(angka.length);
+}
